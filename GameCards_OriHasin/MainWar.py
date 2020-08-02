05d30393ceb489179485bcb35124177cd927137e
OriHasin <68279130+OriHasin@users.noreply.github.com>
2020-08-02 13:24:19 +0300

    You Finished the main , good job

--- GameCards_OriHasin/MainWar.py
+++ GameCards_OriHasin/MainWar.py
@@ -1,16 +1,16 @@
 from GameCards_OriHasin.CardGame import CardGame
 
 #מודול שבו מתרחש התוכנית הראשית
-#AmoundRound,maxIndex , listcards = רשימה של קלפים בסיבוב
+#AmoundRound, listcards = רשימה של קלפים בסיבוב
 Game=CardGame()
 AmountRound=0
 for y in range(1,6): #לולאת המשחק - 5 סיבובים
     listcards = []
-    for i in Game.listPlayers: # לולאת שליפת קלפים ועדכון סכומים בכל סיבוב
+    for i in Game.listPlayers: #לולאת שליפת קלפים מהשחקנים בכל סיבוב ועדכון הסכומים
         listcards.append(i.getCard())
         i.reduceAmount(100*y)
     AmountRound+=(100*y)*4
-    if listcards[0]>listcards[1]: #בודק את הקלף הגדול ביותר ( שימוש בפונקצית __gt__)
+    if listcards[0]>listcards[1]: #בדיקה של הקלף המנצח בסיבוב - חלק א' - חצי גמר ( שימוש בפונקצית __gt__)
         maxcard1=listcards[0]
         maxIndex=0
     else:
@@ -22,15 +22,15 @@ for y in range(1,6): #לולאת המשחק - 5 סיבובים
     else:
         maxcard2=listcards[3]
         maxIndex2=3
-    if maxcard1>maxcard2:
-        Game.listPlayers[maxIndex].addAmount(AmountRound)  #הוספת סכום בקופת סיבוב לשחקן בעל הקלף הזוכה
+    if maxcard1>maxcard2: #בדיקה של הקלף המנצח בסיבוב - חלק 2 - גמר
+        Game.listPlayers[maxIndex].addAmount(AmountRound)  #הדפסת הקלף המנצח , הוספת סכום הזכייה לשחקן והדפסת הקלפים בסיבוב
         print(f'The cards in round: {listcards} , The Winner Card : {listcards[maxIndex]} ')
     else:
-        Game.listPlayers[maxIndex2].addAmount(AmountRound)  #הוספת סכום בקופת סיבוב לשחקן בעל הקלף הזוכה
+        Game.listPlayers[maxIndex2].addAmount(AmountRound)  #הדפסת הקלף המנצח , הוספת סכום הזכייה לשחקן והדפסת הקלפים בסיבוב
         print(f'The cards in round: {listcards} , The Winner Card : {listcards[maxIndex2]} ')
 
 maxAmount=0
-for i in range(len(Game.listPlayers)): #בודק את השחקן הזוכה
+for i in range(len(Game.listPlayers)): #בדיקת השחקן הזוכה והדפסתו
     if Game.listPlayers[i].Amount>maxAmount:
         maxAmount=Game.listPlayers[i].Amount
         indexAmount=i
